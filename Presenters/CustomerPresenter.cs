e1734439af6b646c98592f6c068e06473d47aee4
JuanMejia <juansebastianmejiagomez31@gmail.com>
2025-04-18 16:51:55 -0500

    Construyendo el Presentador para la vista Customer

---
+++ Presenters/CustomerPresenter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supermarket_mvp.Models;
+using Supermarket_mvp.Views;
+
+namespace Supermarket_mvp.Presenters
+{
+    internal class CustomerPresenter
+    {
+        private ICustomerView view;
+        private ICustomerRepository repository;
+        private BindingSource customerBindingSource;
+        private IEnumerable<CustomerModel> customerList;
+
+        public CustomerPresenter(ICustomerView view, ICustomerRepository repository)
+        {
+            this.customerBindingSource = new BindingSource();
+
+            this.view = view;
+            this.repository = repository;
+
+            this.view.SearchEvent += SearchCustomer;
+            this.view.AddNewEvent += AddNewCustomer;
+            this.view.EditEvent += LoadSelectedCustomerToEdit;
+            this.view.DeleteEvent += DeleteSelectedCustomer;
+            this.view.SaveEvent += SaveCustomer;
+            this.view.CancelEvent += CancelAction;
+
+            this.view.SetCustomerListBindingSource(customerBindingSource);
+
+            LoadAllCustomerList();
+
+            this.view.Show();
+        }
+
+        private void LoadAllCustomerList()
+        {
+            customerList = repository.GetAll();
+            customerBindingSource.DataSource = customerList;
+        }
+
+        private void CancelAction(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SaveCustomer(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DeleteSelectedCustomer(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadSelectedCustomerToEdit(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddNewCustomer(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SearchCustomer(object? sender, EventArgs e)
+        {
+            bool emptyValue = string.IsNullOrWhiteSpace(this.view.SearchValue);
+            if (emptyValue == false)
+            {
+                customerList = repository.GetByValue(this.view.SearchValue);
+            }
+            else
+            {
+                customerList = repository.GetAll();
+            }
+            customerBindingSource.DataSource = customerList;
+        }
+    }
+}
