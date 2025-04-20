cb6ad4fad5930ca600c9841a04720c4920dd5385
JuanMejia <juansebastianmejiagomez31@gmail.com>
2025-04-20 17:51:18 -0500

    Implementando el repositorio de Categories para el manejo de los datos

---
+++ _Repositories/CategoryRepository.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data;
+using Supermarket_mvp.Models;
+using System.Data;
+
+
+namespace Supermarket_mvp._Repositories
+{
+    internal class CategoryRepository : BaseRepository, ICategoryRepository
+    {
+        public CategoryRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public void Add(CategoryModel categoryModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(CategoryModel categoryModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CategoryModel> GetAll()
+        {
+            var categoryList = new List<CategoryModel>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM Categories ORDER BY id DESC";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var categoryModel = new CategoryModel();
+                        categoryModel.Id = (int)reader["id"];
+                        categoryModel.Name = reader["name"].ToString();
+                        categoryModel.Description = reader["description"].ToString();
+                        categoryList.Add(categoryModel);
+                    }
+                }
+            }
+            return categoryList;
+        }
+
+        public IEnumerable<CategoryModel> GetByValue(string value)
+        {
+            var categoryList = new List<CategoryModel>();
+            int categoryId = int.TryParse(value, out _) ? Convert.ToInt32(value) : 0;
+            string categoryName = value;
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"SELECT * FROM Categories
+                                    WHERE id = @id OR name LIKE @name + '%'
+                                    ORDER BY id DESC";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = categoryId;
+                command.Parameters.Add("@name", SqlDbType.Char).Value = categoryName;
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var categoryModel = new CategoryModel();
+                        categoryModel.Id = (int)reader["id"];
+                        categoryModel.Name = reader["name"].ToString();
+                        categoryModel.Description = reader["description"].ToString();
+                        categoryList.Add(categoryModel);
+                    }
+                }
+            }
+            return categoryList;
+        }
+    }
+}
