bcc1f8cb0b1db223bf3c6df61119f9d343434b57
JuanMejia <juansebastianmejiagomez31@gmail.com>
2025-04-20 21:36:20 -0500

    Implementando las funciones de adicionar, editar y eliminar para Product

--- Presenters/ProductPresenter.cs
+++ Presenters/ProductPresenter.cs
@@ -43,27 +43,85 @@ namespace Supermarket_mvp.Presenters
 
         private void CancelAction(object? sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            CleanViewFields();
         }
 
         private void SaveProduct(object? sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            var product = new ProductModel();
+
+            product.Id = 0;
+            product.Name = view.ProductName;
+            product.Price = string.IsNullOrWhiteSpace(view.ProductPrice) ? -1 : Convert.ToInt32(view.ProductPrice);
+            product.Stock = string.IsNullOrWhiteSpace(view.ProductStock) ? -1 : Convert.ToInt32(view.ProductStock);
+            product.CategoryId = string.IsNullOrWhiteSpace(view.ProductCategoryId) ? -1 : Convert.ToInt32(view.ProductCategoryId);
+
+
+
+            try
+            {
+                new Common.ModelDataValidation().Validate(product);
+                if (view.IsEdit)
+                {
+                    repository.Edit(product);
+                    view.Message = "Product edited successfuly";
+                }
+                else
+                {
+                    repository.Add(product);
+                    view.Message = "Product added successfuly";
+                }
+                view.IsSuccessful = true;
+                LoadAllProductList();
+                CleanViewFields();
+            }
+            catch (Exception ex)
+            {
+                view.IsSuccessful = false;
+                view.Message = ex.Message;
+            }
+        }
+        private void CleanViewFields()
+        {
+            view.ProductId = "";
+            view.ProductName = "";
+            view.ProductPrice = "";
+            view.ProductStock = "";
+            view.ProductCategoryId = "";
         }
 
         private void DeleteSelectedProduct(object? sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var product = (ProductModel)productBindingSource.Current;
+                repository.Delete(product.Id);
+                view.IsSuccessful = true;
+                view.Message = "Product deleted successfully";
+                LoadAllProductList();
+            }
+            catch (Exception ex)
+            {
+                view.IsSuccessful = false;
+                view.Message = "An error occurred, could not delete product";
+            }
         }
 
         private void LoadSelectedProductToEdit(object? sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            var product = (ProductModel)productBindingSource.Current;
+            view.ProductId = product.Id.ToString();
+            view.ProductName = product.Name;
+            view.ProductPrice = product.Price.ToString();
+            view.ProductStock = product.Stock.ToString();
+            view.ProductCategoryId = product.CategoryId.ToString();
+
+            view.IsEdit = true;
         }
 
         private void AddNewProduct(object? sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            view.IsEdit = false;
         }
 
         private void SearchProduct(object? sender, EventArgs e)
